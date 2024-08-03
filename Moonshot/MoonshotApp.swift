a17c0e8f310fbebd1572b2958ae20ca2f8eb666b
Khaled Hossameldin <khaled-hossam@outlook.com>
2024-08-03 15:53:26 +0300

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Khaled Hossameldin on 03/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
